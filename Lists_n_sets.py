9519db6b99765cb521fa3de9e4a010e9332c15e5
OQASergey <sergey.qa.63@protonmail.com>
2023-12-04 23:50:27 +0400

    Обновлен файл list_n_sets.py - добавлена операция со вложенным списком и добавлены операции со множествами

--- Lists_n_sets.py
+++ Lists_n_sets.py
@@ -1,3 +1,4 @@
+print("** СПИСКИ **")
 # список (list)
 
 list1 = [1, 2, 3, "a", "b", "c"]
@@ -17,4 +18,26 @@ print(list4)
 list4.sort()
 print(list4) #list4 уже отредактирован безвозвратно
 
+a = [1,2,3,4,[5,6,7]]
+print(a[-1][0:2])
+
+print("** МНОЖЕСТВА **")
 # Множества (sets)
+
+list01 = [1, 2, 3, 3, 3, 4, 4, 5]
+print(list01)
+set01 = set(list01)
+set02 = {5, 5, 3, 7, 9, 12, 12}
+list01_1 = list(set01)
+print(list01_1)
+
+set_un = set01.union(set02)
+print(set_un)
+
+print(set01.intersection(set02))
+print(set01.difference(set02))
+
+a = {1,2,2,3,3,3,4,5}
+b = {2,4,12}
+print(a.difference(b))
+
